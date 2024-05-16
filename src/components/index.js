6750df41faee125bfe726a2ea1c82d05efe6a798
Mehran Ali Tehran <Mehranalee110@gmail.com>
2024-05-16 08:13:17 -0700

    setup

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export {default as Cart} from "./cart/cart";
+export {default as CartButton} from "./cart/cartButton";
+export {default as CartItem} from "./cart/cartItem";
+export {default as Layout} from "./layout/layout";
+export {default as MainHeader} from "./layout/mainHeader";
+export {default as ProductItem} from "./shop/productItem";
+export {default as Products} from "./shop/products";
+export {default as Card} from "./ui/card";
---
+++ src/components/shop/products.js
@@ -0,0 +1,19 @@
+import classes from './products.module.css';
+import ProductItem from './productItem';
+
+const Products = (props) => {
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>
+        <ProductItem
+          title='Test'
+          price={6}
+          description='This is a first product - amazing!'
+        />
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
